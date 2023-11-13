3efd1de645e3a4f5c4c0d9c7714823a6e812efec
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-13 17:37:00 +0700

    Update aria2-dowloader.sh

--- aria2-dowloader.sh
+++ aria2-dowloader.sh
@@ -2,7 +2,11 @@
 
 count=0
 while [ $count -lt 5 ]; do
-  aria2c -d /root/Downloads --max-connection-per-server=16 https://bit.ly/1GB-testfile && rm -r /root/Downloads/* && clear
+  aria2c -d /root/Downloads --max-connection-per-server=16 https://bit.ly/1GB-testfile 
+  sleep 1
+  rm -r /root/Downloads/* 
+  sleep 1
+  clear
   sleep 1
   ((count++))
 done
