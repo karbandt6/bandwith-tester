7a03b8a4bdbd14f593947566c87570ad31fbb0a8
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-13 19:56:42 +0700

    Update aria2-dowloader.sh

--- aria2-dowloader.sh
+++ aria2-dowloader.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 count=0
-while [ $count -lt 5 ]; do
-  aria2c -d /root/Downloads --max-connection-per-server=16 https://bit.ly/1GB-testfile 
+echo "Skrip ini akan terus berjalan tanpa batas waktu."
+
+while true; do
+  ((count++))
+  echo "Looping telah dijalankan sebanyak $count kali."
+  aria2c -d /root/Downloads --max-connection-per-server=16 --console-log-level=error https://bit.ly/1GB-testfile 
   sleep 1
   rm -r /root/Downloads/* 
   sleep 1
   clear
   sleep 1
-  ((count++))
 done
