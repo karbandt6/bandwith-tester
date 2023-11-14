b4bb09608e6f9adc3125ebda787ef8306d8f6f2b
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-14 10:12:44 +0700

    Update aria2-dowloader.sh

--- aria2-dowloader.sh
+++ aria2-dowloader.sh
@@ -6,7 +6,7 @@ echo "Skrip ini akan terus berjalan tanpa batas waktu."
 while true; do
   ((count++))
   echo "Looping telah dijalankan sebanyak $count kali."
-  aria2c -d /root/Downloads --max-connection-per-server=16 --console-log-level=error https://bit.ly/1GB-testfile 
+  aria2c -d /root/Downloads --max-connection-per-server=16 --console-log-level=error https://ash-speed.hetzner.com/100MB.bin 
   sleep 1
   rm -r /root/Downloads/* 
   sleep 1
