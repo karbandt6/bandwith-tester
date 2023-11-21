5f39bcff96a24ab7d5438c63890e314056ef5cff
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-21 08:25:45 +0700

    Create test-limit-wget.sh

---
+++ test-limit-wget.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+counter=0
+
+while true; do
+    ((counter++))
+    echo "Running the script for the $counter time(s)"
+    
+    rm -rf /root/download/*
+    wget -P /root/download https://bit.ly/1GB-testfile
+    rm -rf /root/download/*
+    
+    sleep 1  # Tunggu 1 detik sebelum mengulang
+done
