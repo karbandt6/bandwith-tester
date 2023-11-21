2199edeef0a73ee53f66ab5dfc61e6a480c533c6
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-21 08:27:55 +0700

    Update test-limit-wget.sh

--- test-limit-wget.sh
+++ test-limit-wget.sh
@@ -9,6 +9,7 @@ while true; do
     rm -rf /root/download/*
     wget -P /root/download https://bit.ly/1GB-testfile
     rm -rf /root/download/*
+    clear
     
     sleep 1  # Tunggu 1 detik sebelum mengulang
 done
