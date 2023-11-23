0b684769be131a3f172b0fa18497f9df79fc11b5
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-23 12:36:47 +0700

    Update test-limit-wget.sh

--- test-limit-wget.sh
+++ test-limit-wget.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-counter=0
+for i in {1..5}; do
+    screen -dmS bw$i bash -c "
+        counter=0
+        download_path=/root/download/$i
 
-while true; do
-    ((counter++))
-    echo "Running the script for the $counter time(s)"
-    
-    rm -rf /root/download/*
-    wget -P /root/download https://ash-speed.hetzner.com/100MB.bin
-    rm -rf /root/download/*
-    clear
-    
-    sleep 1  # Tunggu 1 detik sebelum mengulang
+        while true; do
+            ((counter++))
+            echo \"Running the script for the \$counter time(s) in session bw$i\"
+
+            rm -rf \$download_path/*
+            wget -P \$download_path https://mirror.nforce.com/pub/speedtests/multi_100mb.bin.1
+            rm -rf \$download_path/*
+            clear
+
+            sleep 1  # Tunggu 1 detik sebelum mengulang
+        done
+    "
 done
