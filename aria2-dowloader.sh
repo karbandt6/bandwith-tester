dda38d318866c53dfc281ce6cef1e373589632e3
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2023-11-25 18:14:03 +0700

    Update aria2-dowloader.sh

--- aria2-dowloader.sh
+++ aria2-dowloader.sh
@@ -7,7 +7,7 @@ while true; do
   ((count++))
   echo "Looping telah dijalankan sebanyak $count kali."
   rm -rf /root/Downloads/*
-  aria2c -d /root/Downloads --max-connection-per-server=16 --console-log-level=error https://bit.ly/1GB-testfile 
+  aria2c -d /root/Downloads --max-connection-per-server=16 --console-log-level=error https://bin.kartolo.cloud/100MB.bin 
   sleep 1
   rm -rf /root/Downloads/* 
   sleep 1
